54f99a13cf0878123d9d9a4f5d794cf1424d63a3
TaherAfini <afinitaher@gmail.com>
2023-04-04 20:00:01 -0400

    Added next button

--- draftorder.js
+++ draftorder.js
@@ -1,27 +1,39 @@
-$("document").ready(() => {
-    $("button").click(() => {
-        $("button").remove()
-        $(".list").css('display', 'block')
-        draft()
-    })
+$("#generate").click(() => {
+    $("#generate").remove()
+    $(".list").css('display', 'block')
+    $("#next").css('display', 'block')
+    draft()
 })
 
+$("#next").click(() => {
+    promise = true;
+})
+
+let promise = false;
 
 const draft = async() => {
 
-    let captains = ['Burhanuddin Danish', 'Taha Lokhandwala', 'Ebraheem Raja', 'Ammar Zumkhawala', 'Ahmed Hussain', 'Mohammed Danish', 'Husain Udaipurwala', 'Hussain Bhagat', 'Mansoor Poonawala', 'Husain Attarwala']
+    let captains = ['Burhanuddin Danish', 'Taha Lokhandwala', 'Ebraheem Raja', 'Ammar Zumkhawala', 'Ahmed Hussain', 'Mohammed Danish', 'Husain Udaipurwala', 'Hussain Bhagat', 'Mansoor Poonawala', 'Husain Attarwala', 'Taha Bhai Salim', 'Burhanuddin Bhai Tambawala']
 
     let captainsLeft = [...captains]
     
     while (captainsLeft.length > 0) {
         let i = Math.floor(Math.random()*captainsLeft.length)
         let captain = captainsLeft[i]
-        let captainName = $("<li></li>").text(captain)
-        $("#captainList").append(captainName) 
-        console.log(captain)
-        await new Promise(r => setTimeout(r, 2000));
+        let captainName = $("<h3></h3>").text(captain)
+        $(".list").append(captainName) 
         captainsLeft = captainsLeft.filter((e) => {
             return e != captain
         })
+        while(!promise) {
+            await new Promise(r => setTimeout(r, 50))
+        }
+        promise = false
+
+        if(captainsLeft.length == 1) {
+            $("#next").css('display', 'none')
+        }
     }
+
+    
 }
